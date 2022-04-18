282dabcb5c23021ef339b3f94157784c07b5ac61
mhindupur <mhindupur@hotmail.com>
2022-04-18 18:18:00 +0530

    adding \n

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
-print "Hello World, This is App1Ind -  Manjunath India";
-<br/>
-  <br/>
+print 'Hello World, This is App1Ind -  Manjunath India\n\n';
 
 print "Manjunath adding this print URL";
