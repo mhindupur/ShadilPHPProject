a8e043f452beff68a55fec0217b9ae673c4d5fc4
mhindupur <mhindupur@hotmail.com>
2022-04-18 18:22:38 +0530

    Simple things

--- index.php
+++ index.php
@@ -3,4 +3,6 @@
 print "Hello World, This is App1Ind -  Manjunath India\r\n";
 
 
-print "Manjunath adding this print URL";
+print "Manjunath adding this print URL\r\n";
+
+print "simple things writing here."
